0393855b31dd14172f32fbef2b3e413de1022ec4
sovigod <CrashDGC@gmail.com>
2021-02-17 09:51:08 +0100

    0.2.1

--- prototypes/techfixes.lua
+++ prototypes/techfixes.lua
@@ -4,5 +4,5 @@ bobmods.lib.tech.replace_science_pack("gun-turret", "angels-science-pack-red", "
 bobmods.lib.tech.replace_science_pack("gun-turret", "datacore-war-1", "datacore-basic")
 
 -- фикс снайперскийх турелей
-bobmods.lib.tech.replace_prerequisite("bob-sniper-turrets-1", "tech-specialised-labs-basic-war-2", "tech-specialised-labs-basic-war-1")
+bobmods.lib.tech.replace_prerequisite("bob-sniper-turrets-1", "tech-specialised-labs-basic-war-2", "military")
 bobmods.lib.tech.replace_science_pack("bob-sniper-turrets-1", "angels-science-pack-green", "angels-science-pack-red")
