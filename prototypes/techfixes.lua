c9f5093390ddc6e8086e11003ae439d96a48bddf
sovigod <CrashDGC@gmail.com>
2021-02-17 09:15:46 +0100

    init commit

---
+++ prototypes/techfixes.lua
@@ -0,0 +1,8 @@
+-- фикс мелких турелей
+bobmods.lib.tech.replace_prerequisite("gun-turret", "tech-specialised-labs-basic-war-1", "angels-components-weapons-basic")
+bobmods.lib.tech.replace_science_pack("gun-turret", "angels-science-pack-red", "angels-science-pack-grey")
+bobmods.lib.tech.replace_science_pack("gun-turret", "datacore-war-1", "datacore-basic")
+
+-- фикс снайперскийх турелей
+bobmods.lib.tech.replace_prerequisite("bob-sniper-turrets-1", "tech-specialised-labs-basic-war-2", "tech-specialised-labs-basic-war-1")
+bobmods.lib.tech.replace_science_pack("bob-sniper-turrets-1", "angels-science-pack-green", "angels-science-pack-red")
